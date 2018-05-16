d4793e57c0a60408e96795e60dbf40e349c3fef0
Tara Johnson <tara@madface.com>
2018-05-16 14:11:47 -0700

    Updates from code review - in progress

--- js/app.js
+++ js/app.js
@@ -3,13 +3,18 @@
 // global variables
 var allProducts = [];
 
-var imgEl1 = document.getElementById('product1');
-var imgEl2 = document.getElementById('product2');
-var imgEl3 = document.getElementById('product3');
+var imgEl1 = document.getElementById('image-1');
+var imgEl2 = document.getElementById('image-2');
+var imgEl3 = document.getElementById('image-3');
+var sectionEl = document.getElementById('click-tracker-container');
+var resultUl = document.getElementById('product-result-container');
 
 var product1Index = 0;
 var product2Index = 0;
 var product3Index = 0;
+
+var totalClicks = 0;
+
 // --------------------------------------------------
 
 // object constructor for products
@@ -20,6 +25,10 @@ function Product (src, name) {
     this.clicked = 0;
     allProducts.push(this); // pushes information into allProducts array
 }
+
+// ------------------------------
+// Event Listeners
+// ------------------------------
 
 // eventListener to count how many times a product in product1index is shown
 imgEl1.addEventListener('load', function(){
@@ -36,59 +45,98 @@ imgEl1.addEventListener('load', function(){
     allProducts[product3Index].shown++;
 })
 
-// eventListener to count how many times a product in product1index is clicked on
-imgEl1.addEventListener('click', function(){
-    // allProducts[product1Index].shown++;
+imgEl1.addEventListener('click', img1Callback);
+
+function img1Callback(){
     allProducts[product1Index].clicked++;
-    // if (allProducts[product1Index].clicked === 2){
-    //     alert('2 clicks');
-    //     }
-    // console.log(allProducts[product1Index].clicked)
-    // chooseNewProduct function runs when item 1 is clicked on
+    totalClicks++;
     chooseNewProduct();
-})
+}
 
-// eventListener to count how many times a product in product2index is clicked on
-imgEl2.addEventListener('click', function(){
-    // allProducts[product1Index].shown++;
+imgEl1.addEventListener('click', img2Callback);
+
+function img2Callback(){
     allProducts[product2Index].clicked++;
-    // chooseNewProduct function runs when item 2 is clicked on
+    totalClicks++;
     chooseNewProduct();
-})
+}
 
-// eventListener to count how many times a product in product3index is clicked on
-imgEl3.addEventListener('click', function(){
-    // allProducts[product1Index].shown++;
+imgEl1.addEventListener('click', img3Callback);
+
+function img3Callback(){
     allProducts[product3Index].clicked++;
-    // chooseNewProduct function runs when item 3 is clicked on
+    totalClicks++;
     chooseNewProduct();
-})
+}
+
+// ------------------------------
+// Helper Functions
+// ------------------------------
 
 // function randomly displays images in each of the three containers
 function chooseNewProduct(){
-    product1Index = Math.floor(Math.random() * allProducts.length);
+    checkTotalClicks();
+
+    var cantBeThis = [product1Index, product2Index, product3Index];
+    // console.log(cantBeThis);
+    // var previous1 = product1Index; // 0
+    // var previous2 = product2Index; // 1
+    // var previous3 = product3Index; // 2
+
+    // do{
+    //     product1Index = Math.floor(Math.random() * allProducts.length);
+
+    // } while (previous1 === product1Index || previous2 === product1Index || previous3 === product1Index);
+
+    do{
+        product1Index = Math.floor(Math.random() * allProducts.length);
+    } while (cantBeThis.includes(product1Index));
+   
+    cantBeThis.push(product1Index);
+
+    // do{
+    //     product2Index = Math.floor(Math.random() * allProducts.length);
+    // } while (previous1 === product2Index || previous2 === product2Index || previous3 === product2Index || product1Index === product2Index);
+
+    do{
+        product2Index = Math.floor(Math.random() * allProducts.length);
+    } while (cantBeThis.includes(product2Index));
+
+    cantBeThis.push(product2Index);
+
+    // do{
+    //     product3Index = Math.floor(Math.random() * allProducts.length);
+    // } while (previous1 === product3Index || previous2 === product3Index || previous3 === product3Index || product1Index === product2Index || product2Index === product3Index);
+
+    do{
+        product3Index = Math.floor(Math.random() * allProducts.length);
+    } while (cantBeThis.includes(product3Index));
+
     imgEl1.src = allProducts[product1Index].src;
-    product2Index = Math.floor(Math.random() * allProducts.length);
     imgEl2.src = allProducts[product2Index].src;
-    product3Index = Math.floor(Math.random() * allProducts.length);
     imgEl3.src = allProducts[product3Index].src;
 }
 
-
-
-function calcTotalClicksPerIndex(){
-    var totalClicksPerIndex = 0;
+function renderResults(){
     for (var i in allProducts){
-        console.log(allProducts[i].clicked);
-        for (var j in allProducts){
-            console.log(allProducts[j].clicked);
-            totalClicksPerIndex += allProducts[j][i].clicked;
-            console.log('success');
-        }
+        var newliEl = document.createElement('li');
+        newliEl.textContent = allProducts[i].name + ' clicked :' + allProducts[i].clicked + ' times';
+        resultUlEl.appendChild(newLiEl);
     }
 }
 
+function checkTotalClicks(){
+    if(totalClicks === 25){
+        
+        renderResults();
 
+        imgEl1.removeEventListener('click', img1Callback);
+        
+        imgEl2.removeEventListener('click', img2Callback);
+        
+        imgEl3.removeEventListener('click', img3Callback);
+    }
+}
 
 // create new product objects
 new Product('img/bag.jpg', 'R2D2 suitcase');
@@ -105,4 +153,3 @@ new Product('img/pet-sweep.jpg', 'pet-sweep');
 new Product('img/scissors.jpg', 'scissors');
 
 chooseNewProduct();
-calcTotalClicksPerIndex();

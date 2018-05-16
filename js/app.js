c0ba07d019b1054abdc6a55a554b39e695d2401f
Tara Johnson <tara@madface.com>
2018-05-16 14:44:53 -0700

    Pair program work with Luther and path updates

--- js/app.js
+++ js/app.js
@@ -1,155 +1,191 @@
 'use strict';
+// Global Variables
+var allPictures = [];
 
-// global variables
-var allProducts = [];
-
-var imgEl1 = document.getElementById('image-1');
-var imgEl2 = document.getElementById('image-2');
-var imgEl3 = document.getElementById('image-3');
+var img1 = document.getElementById('image-1');
+var img2 = document.getElementById('image-2');
+var img3 = document.getElementById('image-3');
 var sectionEl = document.getElementById('click-tracker-container');
 var resultUl = document.getElementById('product-result-container');
 
-var product1Index = 0;
-var product2Index = 0;
-var product3Index = 0;
+var picture1index = 0;
+var picture2index = 0;
+var picture3index = 0;
 
 var totalClicks = 0;
+// =========== Chart
+var voteChart = [];
+var busName = [];
 
-// --------------------------------------------------
+//=============================
+function Picture(src, name) {
+  this.url = src;
+  this.name = name;
+  this.clicked = 0;
 
-// object constructor for products
-function Product (src, name) {
-    this.src = src; // url to image file
-    this.name = name; // product name
-    this.shown = 0;
-    this.clicked = 0;
-    allProducts.push(this); // pushes information into allProducts array
+  allPictures.push(this);
 }
 
-// ------------------------------
-// Event Listeners
-// ------------------------------
+function updateChartArrays() {
+  for (var i = 0; i < allPictures.length; i++) {
+    // console.log(allPictures);
+    busName[i] = allPictures[i].name;
+    voteChart[i] = allPictures[i].clicked;
+  }
+}
 
-// eventListener to count how many times a product in product1index is shown
-imgEl1.addEventListener('load', function(){
-    allProducts[product1Index].shown++;
-})
+function busMallVote(thisPicture) {
+  for (var i = 0; i < allPictures.length; i++) {
+    if (thisPicture === allPictures[i].clicked) {
+      allPictures[i].voteChart++;
+      updateChartArrays();
+    }
+  }
+}
 
-// eventListener to count how many times a product in product2index is shown
-imgEl1.addEventListener('load', function(){
-    allProducts[product2Index].shown++;
-})
+//Event Listeners
+sectionEl.addEventListener('click', sectionCallback);
 
-// eventListener to count how many times a product in product3index is shown
-imgEl1.addEventListener('load', function(){
-    allProducts[product3Index].shown++;
-})
+function sectionCallback(event) {
+  checkTotalClicks();
 
-imgEl1.addEventListener('click', img1Callback);
-
-function img1Callback(){
-    allProducts[product1Index].clicked++;
+  if(event.target.id){
     totalClicks++;
-    chooseNewProduct();
+    allPictures[event.target.id].clicked++;
+
+    chooseNewPictures();
+  } else {
+    alert('click on an image');
+  }
 }
 
-imgEl1.addEventListener('click', img2Callback);
+document.getElementById('click-tracker-container').addEventListener('click', function(event) {
+  if (event.target.id !== 'click-tracker-container') {
+    busMallVote(event.target.id);
+  }
+});
 
-function img2Callback(){
-    allProducts[product2Index].clicked++;
-    totalClicks++;
-    chooseNewProduct();
+// Helper functions
+// =============new pictures ======================
+function chooseNewPictures() {
+
+  var cantBeThis = [picture1index, picture2index, picture3index];
+  // var previous1 = picture1index; // 0
+  // var previous2 = picture2index; // 1
+  // var previous3 = picture3index; // 2
+
+  do{
+    picture1index = Math.floor(Math.random() * allPictures.length);
+  } while (cantBeThis.includes(picture1index));
+  cantBeThis.push(picture1index);
+
+  do{
+    picture2index = Math.floor(Math.random() * allPictures.length);
+  } while (cantBeThis.includes(picture2index));
+  cantBeThis.push(picture2index);
+
+  do {
+    picture3index = Math.floor(Math.random() * allPictures.length);
+  } while (cantBeThis.includes(picture3index));
+
+  img1.src = allPictures[picture1index].url;
+  img1.id = picture1index; //sets the image id = to the reference of its corresponding object's position in the array of all images
+  img2.src = allPictures[picture2index].url;
+  img2.id = picture2index;
+  img3.src = allPictures[picture3index].url;
+  img3.id = picture3index;
 }
 
-imgEl1.addEventListener('click', img3Callback);
-
-function img3Callback(){
-    allProducts[product3Index].clicked++;
-    totalClicks++;
-    chooseNewProduct();
-}
-
-// ------------------------------
-// Helper Functions
-// ------------------------------
-
-// function randomly displays images in each of the three containers
-function chooseNewProduct(){
-    checkTotalClicks();
-
-    var cantBeThis = [product1Index, product2Index, product3Index];
-    // console.log(cantBeThis);
-    // var previous1 = product1Index; // 0
-    // var previous2 = product2Index; // 1
-    // var previous3 = product3Index; // 2
-
-    // do{
-    //     product1Index = Math.floor(Math.random() * allProducts.length);
-
-    // } while (previous1 === product1Index || previous2 === product1Index || previous3 === product1Index);
-
-    do{
-        product1Index = Math.floor(Math.random() * allProducts.length);
-    } while (cantBeThis.includes(product1Index));
-   
-    cantBeThis.push(product1Index);
-
-    // do{
-    //     product2Index = Math.floor(Math.random() * allProducts.length);
-    // } while (previous1 === product2Index || previous2 === product2Index || previous3 === product2Index || product1Index === product2Index);
-
-    do{
-        product2Index = Math.floor(Math.random() * allProducts.length);
-    } while (cantBeThis.includes(product2Index));
-
-    cantBeThis.push(product2Index);
-
-    // do{
-    //     product3Index = Math.floor(Math.random() * allProducts.length);
-    // } while (previous1 === product3Index || previous2 === product3Index || previous3 === product3Index || product1Index === product2Index || product2Index === product3Index);
-
-    do{
-        product3Index = Math.floor(Math.random() * allProducts.length);
-    } while (cantBeThis.includes(product3Index));
-
-    imgEl1.src = allProducts[product1Index].src;
-    imgEl2.src = allProducts[product2Index].src;
-    imgEl3.src = allProducts[product3Index].src;
-}
+//==================
 
 function renderResults(){
-    for (var i in allProducts){
-        var newliEl = document.createElement('li');
-        newliEl.textContent = allProducts[i].name + ' clicked :' + allProducts[i].clicked + ' times';
-        resultUlEl.appendChild(newLiEl);
-    }
+  for(var i in allPictures){
+    var newLiEl = document.createElement('li');
+    newLiEl.textContent = allPictures[i].name + ' clicked : ' + allPictures[i].clicked + ' Times';
+    resultUl.appendChild(newLiEl);
+  }
 }
 
-function checkTotalClicks(){
-    if(totalClicks === 25){
-        
-        renderResults();
-
-        imgEl1.removeEventListener('click', img1Callback);
-        
-        imgEl2.removeEventListener('click', img2Callback);
-        
-        imgEl3.removeEventListener('click', img3Callback);
-    }
+function checkTotalClicks() {
+  if(totalClicks === 25){
+    drawChart();
+    // renderResults();
+    sectionEl.removeEventListener('click', sectionCallback);
+  }
 }
 
-// create new product objects
-new Product('img/bag.jpg', 'R2D2 suitcase');
-new Product('img/banana.jpg', 'Banana slicer');
-new Product('img/bathroom.jpg', 'Bathroom iPad stand');
-new Product('img/boots.jpg', 'Boots');
-new Product('img/breakfast.jpg', 'Breakfast');
-new Product('img/bubblegum.jpg', 'Meatball bubble gum');
-new Product('img/chair.jpg', 'chair');
-new Product('img/cthulhu.jpg', 'Cthulhu');
-new Product('img/dog-duck.jpg', 'Dog duck');
-new Product('img/pen.jpg', 'pen');
-new Product('img/pet-sweep.jpg', 'pet-sweep');
-new Product('img/scissors.jpg', 'scissors');
+new Picture('img/bag.jpg', 'bag');
+new Picture('img/banana.jpg', 'banana');
+new Picture('img/bathroom.jpg', 'bathroom');
+new Picture('img/boots.jpg', 'boots');
+new Picture('img/breakfast.jpg', 'breakfast');
+new Picture('img/bubblegum.jpg', 'bubblegum');
+new Picture('img/chair.jpg', 'chair');
+new Picture('img/cthulhu.jpg', 'cthulhu');
+new Picture('img/dog-duck.jpg', 'dog-duck');
+new Picture('img/dragon.jpg', 'dragon');
+new Picture('img/pen.jpg', 'pen');
+new Picture('img/pet-sweep.jpg', 'pet-sweep');
+new Picture('img/scissors.jpg', 'scissors');
+new Picture('img/shark.jpg', 'shark');
+new Picture('img/sweep.png', 'sweep');
+new Picture('img/tauntaun.jpg', 'tauntaun');
+new Picture('img/unicorn.jpg', 'unicorn');
+new Picture('img/usb.gif', 'usb');
+new Picture('img/water-can.jpg', 'water-can');
+new Picture('img/wine-glass.jpg', 'wine-glass');
 
-chooseNewProduct();
+chooseNewPictures();
+
+function drawChart() {
+  updateChartArrays();
+
+  var data = {
+    labels: busName, // titles array we declared earlier
+    datasets: [{
+      label: '# of clicks on each product',
+      data: voteChart, // votes array we declared earlier
+      backgroundColor: [
+        'black', 'black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black','black',
+      ],
+      hoverBackgroundColor: [
+        'blue', 'blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue','blue',
+      ]
+    }]
+  };
+
+  var ctx = document.getElementById('busMallResults').getContext('2d');
+  var busMallResults = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: data,
+    options: {
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 1.0,
+          scaleOverride: true,
+          scaleStartValue: 0,
+          scaleSteps: 15,
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 1.0,
+          scaleOverride: true,
+          scaleStartValue: 0,
+          scaleSteps: 15,
+        }
+      }],
+    }
+  });
+}
